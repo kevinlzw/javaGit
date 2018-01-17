2981a5944a4330f10ec903fa9257c6b0af0e6fc0
Zhongwen Lian <klian@colgate.edu>
2018-01-17 19:56:27 +0800

    增加reverse的实现方法

--- java test/src/test.java
+++ java test/src/test.java
@@ -1,4 +1,15 @@
 
 public class test {
 
+	int reverse(int x)  
+	{  
+	    int y=0,z;  
+	    for (; x>0; x=x/10)  
+	    {  
+	        z=x%10;  
+	        y=y*10+z;  
+	    }  
+	    return y;  
+	}  
+	
 }
